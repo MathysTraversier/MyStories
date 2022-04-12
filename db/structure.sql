9d76630b4e9726c9abec77359573302f7369a61c
MathysTraversier <101551406+MathysTraversier@users.noreply.github.com>
2022-04-12 16:08:57 +0200

    bdd

---
+++ db/structure.sql
@@ -0,0 +1,33 @@
+drop table if exists user;
+drop table if exists story;
+drop table if exists step;
+drop table if exists choice;
+
+create table story (
+    sto_id integer not null primary key auto_increment,
+    sto_title varchar(100) not null,
+    sto_description_short varchar(500) not null,
+    sto_description_long varchar(2000) not null,
+    sto_image varchar(150)
+) engine=innodb character set utf8 collate utf8_unicode_ci;
+
+create table user (
+    usr_id integer not null primary key auto_increment,
+    usr_login varchar(50) not null,
+    usr_password varchar(88) not null,
+    usr_lives integer
+) engine=innodb character set utf8 collate utf8_unicode_ci;
+
+create table step (
+    ste_id integer not null primary key auto_increment,
+    ste_description varchar(5000) not null,
+    sto_id integer not null,
+    foreign key (sto_id) REFERENCES story (sto_id)
+) engine=innodb character set utf8 collate utf8_unicode_ci;
+
+create table choice (
+    cho_id integer not null primary key auto_increment,
+    cho_description varchar(2000) not null,
+    ste_id integer not null,
+    foreign key (ste_id) REFERENCES step (ste_id)
+) engine=innodb character set utf8 collate utf8_unicode_ci;
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
+<?php
+require("includes/connect.php");
+?>
 <html lang="fr">
 
 <head>
@@ -6,49 +9,60 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <link href="style.css" rel="stylesheet">
     <title>Document</title>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
+    <nav class="navbar navbar-expand-lg navbar-light bg-#6f42c1">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
+                        <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
+                    </svg></i> MyStories</a>
+            </div>
+            <div class="nav-item dropdown" id="navbar-collapse-target">
+                <ul class="nav navbar-nav navbar-right">
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                            </svg> </span>Non connecté
                         </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="login.php">Se connecter</a></li>
                         </ul>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
-                    </li>
                 </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
             </div>
         </div>
     </nav>
+
+    <div class="container mt-5">
+        <?php
+        $sqlQuery = 'SELECT * FROM story';
+        $storiesStatement = $bdd->prepare($sqlQuery);
+        $storiesStatement->execute();
+        $stories = $storiesStatement->fetchAll();
+
+        foreach ($stories as $story) {
+        ?>
+            <article class="mb-5 story">
+                <img class="rounded float-start" src="images/<?= $story['sto_image'] ?>" width="100px" />
+                <h3><a class="storyTitle" href="story.php?id=<?= $movie['sto_id'] ?>"><?= $story['sto_title'] ?></a></h3>
+                <p class="storyContent"><?= $story['sto_description_long'] ?></p>
+            </article>
+        <?php
+        }
+        ?>
+
+        <hr />
+
+        <footer class="footer text-center">
+            Construit avec ❤ par Léna et Mathys</a>.
+        </footer>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
