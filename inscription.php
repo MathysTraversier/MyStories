b6ebe235810e4f88e715c9b4b9ff1afb3f6531cf
LenaCoulon <101551359+LenaCoulon@users.noreply.github.com>
2022-04-22 09:16:55 +0200

    connexion et inscription
    connexion et inscription

--- inscription.php
+++ inscription.php
@@ -12,44 +12,15 @@
 
 </body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
-                    </li>
-                </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav>
+<nav class="navbar navbar-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#" id="myStories">
+      <img src="images\th-removebg-preview 1.png" alt="" width="45" height="45" class="d-inline-block align-text-top">
+      MyStories
+    </a>
+    <button class="btn btn-sm btn-outline-secondary" type="button">Se connecter</button>
+  </div>
+</nav>
 
 
 <section class="vh-100 align-items-center" >
@@ -57,14 +28,14 @@
     <div class="container h-100">
       <div class="row d-flex justify-content-center align-items-center h-100" id="formulaire">
         <div class="col-12 col-md-9 col-lg-7 col-xl-6">
-          <div class="card" style="border-radius: 15px;">
+          <div class="card" style="border-radius: 20px; ">
             <div class="card-body p-2">
               <h2 class="text-uppercase text-center mb-2 pt-1">CREER UN COMPTE</h2></br>
 
-              <form >
-                <div class="form-outline  w-100 mb-2">
+              <form>
+                <div class="form-outline w-75 ms-3 mb-2">
                 NOM
-                  <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
+                  <input type="text" id="form3Example1cg" class="form-control form-control-lg " />
                   <label class="form-label" for="form3Example1cg"></label>
                 </div>
 
