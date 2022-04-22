e2af954d5e20fcaf77cf1d9f4d89a2966a9cca01
MathysTraversier <101551406+MathysTraversier@users.noreply.github.com>
2022-04-22 09:24:16 +0200

    css changes

---
+++ includes/navbar.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#" id="myStories">
+            <img src="images\th-removebg-preview 1.png" alt="" width="45" height="45" class="d-inline-block align-text-top">
+            MyStories
+        </a>
+        <button class="btn btn-sm btn-outline-secondary" type="button">Se connecter</button>
+    </div>
+</nav>
