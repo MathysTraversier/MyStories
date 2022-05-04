a2ea1ef8ffecd266d349d9b21300533adc255503
MathysTraversier <101551406+MathysTraversier@users.noreply.github.com>
2022-05-04 11:34:49 +0200

    update bdd

--- db/choice.sql
+++ db/choice.sql
@@ -1,22 +1,22 @@
 insert into choice (cho_ste,cho_name,cho_description,ste_id) values
-("A","aube","Partir à l'aube",1);
+("A","aube","Partir à l'aube",2);
 insert into choice (cho_ste,cho_name,cho_description,ste_id) values
-("A","14h","Partir à 14h", 2);
+("A","14h","Partir à 14h", 3);
 insert into choice (cho_ste,cho_name,cho_description,ste_id) values
-("B","sansLeash", "Surfer sans leash", 3);
+("B","sansLeash", "Surfer sans leash", 4);
 insert into choice (cho_ste,cho_name,cho_description,ste_id) values
-("B","avecLeash", "Surfer avec leash", 4);
+("B","avecLeash", "Surfer avec leash", 5);
 insert into choice (cho_ste,cho_name,cho_description,ste_id) values
-("C","avecCreme", "Mettre de la crème solaire", 5);
+("C","avecCreme", "Mettre de la crème solaire", 6);
 insert into choice (cho_ste,cho_name,cho_description,ste_id) values
-("C","sansCreme", "Ne pas mettre de crème solaire", 6);
+("C","sansCreme", "Ne pas mettre de crème solaire", 7);
 insert into choice (cho_ste,cho_name,cho_description,ste_id) values
-("D","prendreVague", "Prendre la vague", 7);
+("D","prendreVague", "Prendre la vague", 8);
 insert into choice (cho_ste,cho_related,cho_name,cho_description,ste_id) values
-("D", 5, "pasPrendreVagueAC", "Ne pas prendre la vague", 8);
+("D", 5, "pasPrendreVagueAC", "Ne pas prendre la vague", 9);
 insert into choice (cho_ste,cho_related,cho_name,cho_description,ste_id) values
-("D", 6, "pasPrendreVagueSC", "Ne pas prendre la vague", 9);
+("D", 6, "pasPrendreVagueSC", "Ne pas prendre la vague", 10);
 insert into choice (cho_ste,cho_name,cho_description,ste_id) values
-("E","colere", "S'énerver et pousser le surfer", 10);
+("E","colere", "S'énerver et pousser le surfer", 11);
 insert into choice (cho_ste,cho_name,cho_description,ste_id) values
-("E","chill", "Rester chill", 11);
+("E","chill", "Rester chill", 12);
