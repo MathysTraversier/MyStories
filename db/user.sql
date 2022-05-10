f2869e3c8761f271a7b19d6622772bda48ca9d9f
LenaCoulon <101551359+LenaCoulon@users.noreply.github.com>
2022-05-10 15:49:22 +0200

    modif db content

--- db/user.sql
+++
@@ -1,4 +0,0 @@
-insert into user (usr_name,usr_email,usr_password,usr_admin) values
-("correcteur","blabla@gmail.com","mdp_correcteur_1234",0);
-insert into user (usr_name,usr_email,usr_password,usr_admin) values
-("correcteur_admin","blabla1@gmail.com","mdp_correcteur_1234",1);
